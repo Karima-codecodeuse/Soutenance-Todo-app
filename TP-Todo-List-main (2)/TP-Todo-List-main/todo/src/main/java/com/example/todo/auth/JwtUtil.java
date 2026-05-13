3c6c6cc99f5ecb5762c7cebb2eba5644415fe164
Your Name <you@example.com>
2026-05-13 17:49:14 +0200

    Todo-app

---
+++ TP-Todo-List-main (2)/TP-Todo-List-main/todo/src/main/java/com/example/todo/auth/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.example.todo.auth;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    // 🔐 Clé secrète (⚠️ à externaliser plus tard dans application.properties ou variable d'environnement)
+    private final String SECRET = "monsecretultrasecurepourjwt123456789";
+    private final long EXPIRATION_TIME = 86400000; // 1 jour en millisecondes
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    // Générer un token
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Extraire le username
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey())
+                .build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Vérifier validité du token
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
---
+++ TP-Todo-List-main (2)/TP-Todo-List-main/todo/src/main/java/com/example/todo/auth/Keys.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.todo.auth;
+
+import java.security.Key;
+
+
+class Keys {
+
+    static Key hmacShaKeyFor(byte[] bytes) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
